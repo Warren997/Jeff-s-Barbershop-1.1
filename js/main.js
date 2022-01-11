7aa94090daf5c3e208d4b34c60832edc7e500c81
Warren Tapawan <warrentapawan997@gmail.com>
2022-01-11 17:22:41 +0800

    Add all file of Jeff's Barbershop 1.1

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function headerScroll() {
+    if (window.pageYOffset == 0) {
+        //Scroll Top
+
+        //Hidden Header - hidden
+        document.querySelector('.hidden-header').style.visibility = 'hidden';
+        //Animation reset
+        document.querySelector('.hidden-header').style.animation = 'none';
+
+    } else if (window.pageYOffset < 88) {
+        //Scroll Down
+
+        //Hidden Header - visible
+        document.querySelector('.hidden-header').style.visibility = 'visible';
+        //Animation fade-in-top
+        document.querySelector('.hidden-header').style.animation = 'fade-in-top 0.6s cubic-bezier(0.390, 0.575, 0.565, 1.000) both';
+    }
+}
+
+
+//testimonials
+var testimonials = document.querySelector('.testimonial-item-container');
+
+function testimonialBtnNext() {
+
+    if (testimonials.style.marginLeft == '') {
+        testimonials.style.marginLeft = '-100%';
+    } else if (testimonials.style.marginLeft == '-100%') {
+        testimonials.style.marginLeft = '-200%';
+    }
+}
+
+function testimonialBtnPrev() {
+
+    if (testimonials.style.marginLeft == '-200%') {
+        testimonials.style.marginLeft = '-100%';
+    } else if (testimonials.style.marginLeft == '-100%') {
+        testimonials.style.marginLeft = '';
+    }
+}
